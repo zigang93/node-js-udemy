f634feb373694e1b464f9a7c4570440efa119710
Zi Gang <tanzigang93@gmail.com>
2017-03-08 12:31:24 +0800

    finish the section 3/12 course 20170308

--- app.js
+++ app.js
@@ -1,38 +1,6 @@
 console.log('Starting app.js')
 
 const fs = require('fs');
-const os = require('os');
-const _ = require('lodash')
+const _ = require('lodash');
+
 const notes = require('./notes.js');
-
-//only filter out same name
-var filteredArray = _.uniq(['Zi Gang', 1, 'Zi Gang' , 2 ,3, 4]);
-console.log(filteredArray);
-
-// console.log(_.isString(true));
-// console.log(_.isString('zi gang'));
-
-// let res = notes.addNote();
-// console.log(res);
-// console.log('result:', notes.add(5, 9));
-
-// var user = os.userInfo();
-// var mem = os.freemem();
-// console.log(mem);
-
-//node 7 write style
-// fs.appendFile('greetings.txt', `Hello ${user.username}! You are ${notes.age}`, (err) => {
-//   if (err) throw err;
-//   console.log('The "data to append" was appended to file!');
-// });
-
-//option node 7
-// fs.appendFileSync('greetings.txt', 'Hello Word!');
-
-
-//Option one for 7.6 node js version
-// fs.appendFile('greetings.txt', 'Hello World!', function (err)) {
-//   if (err) {
-//     console.log('Unable to write the file');
-//   }
-// }
--- notes.js
+++ notes.js
@@ -4,7 +4,3 @@ module.exports.addNote = () => {
   console.log('addNote');
   return 'New note';
 }
-
-module.exports.add = function(a,b) {
-  return a + b ;
-}
