12cc699d20c5960963269c570fb2b85ae0807206
Zi Gang <tanzigang93@gmail.com>
2017-03-08 11:37:12 +0800

    update all file and make ignore git work 20170308

--- app.js
+++ app.js
@@ -2,11 +2,19 @@ console.log('Starting app.js')
 
 const fs = require('fs');
 const os = require('os');
+const _ = require('lodash')
 const notes = require('./notes.js');
 
-let res = notes.addNote();
-console.log(res);
-console.log('result:', notes.add(5, 9));
+//only filter out same name
+var filteredArray = _.uniq(['Zi Gang', 1, 'Zi Gang' , 2 ,3, 4]);
+console.log(filteredArray);
+
+// console.log(_.isString(true));
+// console.log(_.isString('zi gang'));
+
+// let res = notes.addNote();
+// console.log(res);
+// console.log('result:', notes.add(5, 9));
 
 // var user = os.userInfo();
 // var mem = os.freemem();
