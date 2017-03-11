f819e2d4046a424a399d137807ec2b4e06545da0
zigang93 <tanzigang93@gmail.com>
2017-03-12 01:06:47 +0800

    complate section 3 14

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const yargs = require('yargs');
 const notes = require('./notes.js');
 
 const argv = yargs.argv;
-var command = process.argv[2];
+var command = argv._[0];
 console.log('Command: ', command);
 console.log('Yargs', argv);
 
